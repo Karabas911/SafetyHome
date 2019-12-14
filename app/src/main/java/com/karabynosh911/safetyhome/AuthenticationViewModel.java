afb364b420ceb77613a0bd7ce40b1194d0d7e34d
Serhii Karabynosh <karabynosh911@gmail.com>
2019-12-15 00:12:54 +0200

    development(authentication created)

---
+++ app/src/main/java/com/karabynosh911/safetyhome/AuthenticationViewModel.java
@@ -0,0 +1,11 @@
+package com.karabynosh911.safetyhome;
+
+import androidx.lifecycle.ViewModel;
+
+public class AuthenticationViewModel extends ViewModel {
+
+    void authenticate(String userName, String password){
+
+    }
+
+}
