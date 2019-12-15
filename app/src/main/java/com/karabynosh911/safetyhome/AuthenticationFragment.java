5327b471430707e8f0fe125969bb4f7eba5dcbca
Serhii Karabynosh <karabynosh911@gmail.com>
2019-12-15 14:59:16 +0200

    development(ui improvements)

--- app/src/main/java/com/karabynosh911/safetyhome/AuthenticationFragment.java
+++ app/src/main/java/com/karabynosh911/safetyhome/AuthenticationFragment.java
@@ -1,5 +1,6 @@
 package com.karabynosh911.safetyhome;
 
+import androidx.databinding.DataBindingUtil;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
@@ -7,14 +8,21 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHostController;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.karabynosh911.safetyhome.databinding.AuthenticationFragmentBinding;
+
 public class AuthenticationFragment extends Fragment {
 
     private AuthenticationViewModel mViewModel;
+
+    private AuthenticationFragmentBinding binding;
 
     public static AuthenticationFragment newInstance() {
         return new AuthenticationFragment();
@@ -23,7 +31,10 @@ public class AuthenticationFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.authentication_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.authentication_fragment, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+        binding.login.setOnClickListener(v -> navController.navigate(R.id.action_authentication_fragment_to_mainFragment));
+        return binding.getRoot();
     }
 
     @Override
@@ -31,5 +42,4 @@ public class AuthenticationFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(AuthenticationViewModel.class);
     }
-
 }
--- app/src/main/java/com/karabynosh911/safetyhome/PasswordFragment.java
+++ app/src/main/java/com/karabynosh911/safetyhome/PasswordFragment.java
@@ -1,20 +1,25 @@
 package com.karabynosh911.safetyhome;
 
-import androidx.lifecycle.ViewModelProviders;
-
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+
+import com.karabynosh911.safetyhome.databinding.PasswordFragmentBinding;
+
 public class PasswordFragment extends Fragment {
 
     private PasswordViewModel mViewModel;
+
+    private PasswordFragmentBinding binding;
 
     public static PasswordFragment newInstance() {
         return new PasswordFragment();
@@ -23,7 +28,10 @@ public class PasswordFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.password_fragment, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.password_fragment, container, false);
+        NavController navController = NavHostFragment.findNavController(this);
+        binding.proceed.setOnClickListener(v -> navController.navigate(R.id.action_password_fragment_to_mainFragment));
+        return binding.getRoot();
     }
 
     @Override
@@ -31,5 +39,4 @@ public class PasswordFragment extends Fragment {
         super.onActivityCreated(savedInstanceState);
         mViewModel = ViewModelProviders.of(this).get(PasswordViewModel.class);
     }
-
 }
